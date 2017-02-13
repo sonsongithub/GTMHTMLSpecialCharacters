7ca2a62e346a47df0a05a2ec678a8666ebec138c
sonson <yoshida.yuichi@gmail.com>
2017-02-13 09:06:03 +0900

    Added test code.

--- Tests/GTMHTMLSpecialCharactersTests/GTMHTMLSpecialCharactersTests.swift
+++ Tests/GTMHTMLSpecialCharactersTests/GTMHTMLSpecialCharactersTests.swift
@@ -2,15 +2,37 @@ import XCTest
 @testable import GTMHTMLSpecialCharacters
 
 class GTMHTMLSpecialCharactersTests: XCTestCase {
-    func testExample() {
-        let a = "aaa$$&&"
-        print(a.gtm_stringByEscapingForHTML())
-    }
 
+    let testCount = 1000
+    let stringToBeUnescaped = "&quot;&amp;&amp;apos;&lt;&gt;&OElig;&oelig;&Scaron;&scaron;&Yuml;&circ;&tilde;&ensp;&emsp;&thinsp;&zwnj;&zwj;&lrm;&rlm;&ndash;&mdash;&lsquo;&rsquo;&sbquo;&ldquo;&rdquo;&bdquo;&dagger;&Dagger;&permil;&lsaquo;&rsaquo;&euro;hoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahoghoge&copy;a&copy;aaaaa&copy;aaaaahog"
+    let stringToBeEscaped = "\"&&apos;<>ŒœŠšŸˆ˜   ‌‍‎‏–—‘’‚“”„†‡‰‹›€hoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahoghoge©a©aaaaa©aaaaahog"
+    
+    func testStringRoundtrippingEscapedHTML() {
+        let string = "This test ©™®๒०᠐٧"
+        XCTAssert(string.gtm_stringByEscapingForHTML().gtm_stringByUnescapingFromHTML() == string)
+    }
+    
+    func testEscapePerformance() {
+        self.measure {
+            for _ in 0..<self.testCount {
+                let _ = self.stringToBeUnescaped.gtm_stringByUnescapingFromHTML()
+            }
+        }
+    }
+    
+    func testUnescapePerformance() {
+        self.measure {
+            for _ in 0..<self.testCount {
+                let _ = self.stringToBeEscaped.gtm_stringByEscapingForHTML()
+            }
+        }
+    }
 
     static var allTests : [(String, (GTMHTMLSpecialCharactersTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testStringRoundtrippingEscapedHTML", testStringRoundtrippingEscapedHTML),
+            ("testEscapePerformance", testEscapePerformance),
+            ("testUnescapePerformance", testUnescapePerformance),
         ]
     }
 }
